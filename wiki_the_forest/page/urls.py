57ef19b68f53017a07527cd4c7129fc3987ffe6e
Fabian Vargas <fabian3vargas@gmail.com>
2025-04-04 13:24:23 -0300

    first

---
+++ wiki_the_forest/page/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    path('', views.home , name='home'),
+    path('foro/', views.foro , name='foro'),
+    path('registrarse/', views.registrarse , name='registrarse'),
+    path('inicio_sesion/', views.inicio_sesion , name='inicio_sesion'),
+    path('mi_cuenta/', views.mi_cuenta , name='mi_cuenta'),
+
+    
+]
---
+++ wiki_the_forest/page/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def foro(request):
+    return render(request, 'forowiki.html')
+
+def registrarse(request):
+    return render(request, 'registrase_wiki.html')
+
+def inicio_sesion(request):
+    return render(request, 'inicio_sesion_wiki.html')
+
+def mi_cuenta(request):
+    return render(request, 'micuentatf.html')
